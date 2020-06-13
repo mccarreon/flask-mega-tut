23b205d869dd5c96017be38251d0e71b7d55856f
Matt Carreon <mattc.carreon@gmail.com>
2020-06-12 23:42:44 -0700

    chapter one and two

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index') 
+def index(): 
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Bildo'},
+            'body': 'The Avengers movie was soddf cool!'
+        },
+        {
+            'author': {'username': 'Rach'},
+            'body': 'The Avengers movie was soooo cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
